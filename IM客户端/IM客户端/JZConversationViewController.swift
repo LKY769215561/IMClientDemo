3b8f352369dfbedc2f877ee4d008e4d5ba24f8ec
Kerain123 <kerain.liang@jiuzhang.net>
2017-05-17 10:05:41 +0800

    集成聊天UI框架

---
+++ IM客户端/IM客户端/JZConversationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JZConversationViewController.swift
+//  IM客户端
+//
+//  Created by 梁奎元 on 2017/5/16.
+//  Copyright © 2017年 九章软件. All rights reserved.
+//
+
+import UIKit
+
+class JZConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IM客户端/IM客户端/NotificationName.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationName.swift
+//  IM客户端
+//
+//  Created by 梁奎元 on 2017/5/16.
+//  Copyright © 2017年 九章软件. All rights reserved.
+//
+
+import Foundation
